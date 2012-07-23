310a24d9c176f1e020166993dbbf6ca98770bc4d
Danylo Vashchilenko <dan.vashchilenko@gmail.com>
2012-07-23 12:31:30 +0300

    init runtime persistence config

--- src/census/business/StorageService.java
+++ src/census/business/StorageService.java
@@ -4,7 +4,11 @@
  */
 package census.business;
 
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Observable;
+import java.util.ResourceBundle;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
@@ -12,43 +16,55 @@ import javax.persistence.Persistence;
  *
  * @author Danylo Vashchilenko
  */
-public class StorageService extends Observable{
-    
+public class StorageService extends Observable {
+
     private EntityManager entityManager;
     private static StorageService instance;
-    
+
     protected StorageService() {
-        entityManager = Persistence.createEntityManagerFactory("Census PU")
-                .createEntityManager();
+
+        ResourceBundle config = ResourceBundle.getBundle("etc/mysql");
+
+        Map<String, String> properties = new HashMap<>();
+        properties.put("javax.persistence.jdbc.url",
+                MessageFormat.format("jdbc:mysql://{0}:{1}/{2}?useUnicode=true&amp;connectionCollation=utf8_general_ci&amp;characterSetResults=utf8",
+                config.getString("host"),
+                config.getString("port"),
+                config.getString("database")));
+
+        properties.put("javax.persistence.jdbc.password", config.getString("password"));
+        properties.put("javax.persistence.jdbc.user", config.getString("user"));
+
+        entityManager = Persistence.createEntityManagerFactory("Census PU", properties).createEntityManager();
     }
-    
+
     public EntityManager getEntityManager() {
         return entityManager;
     }
-    
+
     public void beginTransaction() throws IllegalStateException {
         entityManager.getTransaction().begin();
     }
-    
+
     public Boolean isTransactionActive() {
         return entityManager.getTransaction().isActive();
     }
-    
+
     public void commitTransaction() {
         entityManager.getTransaction().commit();
         setChanged();
         notifyObservers();
     }
-    
+
     public void rollbackTransaction() {
         entityManager.getTransaction().rollback();
     }
-    
+
     public static StorageService getInstance() {
-        if(instance == null) {
+        if (instance == null) {
             instance = new StorageService();
         }
-        
+
         return instance;
     }
 }
